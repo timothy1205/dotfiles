8a2a488f098e329401ca264a4172830c1e5aa2a9
timothy1205 <timothy120500@gmail.com>
2020-06-18 12:23:42 -0600

    More dynamic directory path

--- .VFIOinput/input_detach.sh
+++ .VFIOinput/input_detach.sh
@@ -1,2 +1,2 @@
-virsh detach-device w10 /home/timothy/.VFIOinput/input_mouse.xml
-virsh detach-device w10 /home/timothy/.VFIOinput/input_keyboard.xml
+virsh detach-device w10 $HOME/.VFIOinput/input_mouse.xml
+virsh detach-device w10 $HOME/.VFIOinput/input_keyboard.xml
