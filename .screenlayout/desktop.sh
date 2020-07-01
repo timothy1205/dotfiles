12b67bac08b7205600f0f3a2da380bf60e33c354
timothy1205 <timothy120500@gmail.com>
2020-06-30 22:51:09 -0600

    monitor names

--- .screenlayout/desktop.sh
+++ .screenlayout/desktop.sh
@@ -1,2 +1,2 @@
 i!/bin/sh
-xrandr --output DisplayPort-3 --mode 2560x1440 --rate 144 --pos 2560x1080 --rotate normal --output DisplayPort-4 --mode 1920x1080 --rate 75 --pos 1546x0 --rotate normal --output DisplayPort-5 --primary --mode 2560x1440 --rate 144 --pos 0x1080 --rotate normal --output HDMI-A-0 --off
+xrandr --output DisplayPort-0 --mode 2560x1440 --rate 144 --pos 2560x1080 --rotate normal --output DisplayPort-1 --mode 1920x1080 --rate 75 --pos 1546x0 --rotate normal --output DisplayPort-2 --primary --mode 2560x1440 --rate 144 --pos 0x1080 --rotate normal --output HDMI-A-0 --off
