363da9db8806d78ef1f82c9c6f0215ce191558b0
timothy1205 <timothy120500@gmail.com>
2020-11-11 20:35:38 -0700

    New monitor setup

--- .screenlayout/desktop.sh
+++ .screenlayout/desktop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --mode 2560x1440 --rate 144 --pos 2560x1080 --rotate normal --output DisplayPort-1 --mode 1920x1080 --rate 75 --pos 1546x0 --rotate normal --output DisplayPort-2 --primary --mode 2560x1440 --rate 144 --pos 0x1080 --rotate normal --output HDMI-A-0 --off
+xrandr --output DisplayPort-0 --primary --mode 2560x1440 --rate 144 --pos 0x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-2 --mode 2560x1440 --rate 144 --pos 5120x0 --rotate normal --output HDMI-A-0 --mode 2560x1440 --rate 143.97 --pos 2560x0 --rotate normal
