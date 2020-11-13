2b295642fb643c3b7b263054b2c3314262823f81
timothy1205 <timothy120500@gmail.com>
2020-11-13 15:23:16 -0700

    Account for monitor names changing Xrandr provider modesetting randomly changed my monitor names. Check for which one is being used, act accordingly for xrandr and bspwmrc/polybar.

--- .screenlayout/desktop.sh
+++ .screenlayout/desktop.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --primary --mode 2560x1440 --rate 144 --pos 0x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-2 --mode 2560x1440 --rate 144 --pos 5120x0 --rotate normal --output HDMI-A-0 --mode 2560x1440 --rate 143.97 --pos 2560x0 --rotate normal
+
+if xrandr --listactivemonitors | grep DisplayPort
+then
+  xrandr --output DisplayPort-0 --primary --mode 2560x1440 --rate 144 --pos 0x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-2 --mode 2560x1440 --rate 144 --pos 5120x0 --rotate normal --output HDMI-A-0 --mode 2560x1440 --rate 143.97 --pos 2560x0 --rotate normal
+else
+  xrandr --output DP-1 --primary --mode 2560x1440 --rate 144 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --mode 2560x1440 --rate 144 --pos 5120x0 --rotate normal --output HDMI-1 --mode 2560x1440 --rate 143.97 --pos 2560x0 --rotate normal
+fi
